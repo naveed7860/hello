7bb605aff06fce72a1ed9c3c95323a3b5e9f77b0
shaik naveed ahamed <44160062+naveed7860@users.noreply.github.com>
2019-07-04 09:46:35 +0530

    Update and rename naveed6.py to naveed7.py

--- naveed6.py
+++
@@ -1,5 +0,0 @@
-def reverse(string):
-  string="".join(reversed(string))
-  return string
-b=input()
-print(reverse(b))
---
+++ naveed7.py
@@ -0,0 +1,6 @@
+c=int(input())
+if(c%4==0):
+  print("yes")
+else:
+  print("no")
+
